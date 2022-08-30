3bbbd92082bfb8c9967084c8405674be16c80693
Vaibhav Dahale <vaibhavmdhl@gmail.com>
2022-08-30 07:20:02 +0530

    Adding new APIs in app.py

--- app.py
+++ app.py
@@ -5,3 +5,16 @@ app=Flask(__name__)
 @app.route('/')
 def index():
     return 'modified file'
+
+@app.route('/index1')
+def index1():
+    return 'modified file' ## API 1
+
+@app.route('/index2')
+def index2():
+    return 'modified file' ## API 2
+
+@app.route('/index2')
+def index2():
+    return 'modified file' ## API 3
+
